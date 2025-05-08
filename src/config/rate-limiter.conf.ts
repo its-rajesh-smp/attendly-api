561498557f906476bef6f926ad02e69fe4879035
its.rajesh.smp <its.rajeshsmp@gmail.com>
2025-05-09 01:40:40 +0530

    fix: fixed rate-limiter-config

--- src/config/rate-limiter.conf.ts
+++ src/config/rate-limiter.conf.ts
@@ -1,8 +1,10 @@
 import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-8",
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 });
 
 export default limiter;
